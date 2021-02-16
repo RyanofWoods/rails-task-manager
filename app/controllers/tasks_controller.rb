d037f572e2a3f8385d3a56b62745182ba0315f27
Ryan Woods <contact@ryanofwoods.com>
2021-02-16 13:29:40 +0100

    Added new to tasks, routes, controller and view as well as a task form template for DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,9 +8,23 @@ class TasksController < ApplicationController
   def show 
   end
 
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(strong_params)
+
+    redirect_to task_path(@task)
+  end
+
   private
 
   def find_task
     @task = Task.find(params[:id])
+  end
+
+  def strong_params
+    params.require(:task).permit(:title, :details)
   end
 end
